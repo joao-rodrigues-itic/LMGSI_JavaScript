800bfd262f015d764f6a554c769d1e6d99b95805
SergioCordero77 <sergio.cordero77@gmail.com>
2026-02-02 19:04:08 +0100

    Comentarios en javascript

--- script/script.js
+++ script/script.js
@@ -1,14 +1,15 @@
 // Esconder/Mostrar contraseña
-document.getElementById("esconder").addEventListener("change", function() {
-    let contrasena = document.getElementById("contrasena");
+document.getElementById("esconder").addEventListener("change", function() { //Función que cambia cuando se cambia el checkbox
+    let contrasena = document.getElementById("contrasena"); // Se obtiene el input de la contraseña
 
-    if (contrasena.type === 'password') {
+    if (contrasena.type === 'password') { 
         contrasena.setAttribute('type', 'text');
     }else {
         contrasena.setAttribute('type', 'password');
     }
 });
 
+// Referencias a los elementos HTML necesarios para la validación de la contraseña
 var input = document.getElementById("contrasena");
 var minuscula = document.getElementById("minuscula");
 var mayuscula = document.getElementById("mayuscula");
@@ -29,53 +30,54 @@ input.onblur = function() {
 input.onkeyup = function() {
   // Validar letras minúsculas
   var lowerCaseLetters = /[a-z]/g;
-  if (input.value.match(lowerCaseLetters)) {
+  if (input.value.match(lowerCaseLetters)) { //Si la contraseña contiene minúsculas
     minuscula.classList.remove("invalid");
     minuscula.classList.add("valid");
-  } else {
+  } else { //Si la contraseña NO contiene minúsculas
     minuscula.classList.remove("valid");
     minuscula.classList.add("invalid");
   }
 
   // Validar letras mayúsculas
   var upperCaseLetters = /[A-Z]/g;
-  if (input.value.match(upperCaseLetters)) {
+  if (input.value.match(upperCaseLetters)) { //Si la contraseña contiene mayúsculas
     mayuscula.classList.remove("invalid");
     mayuscula.classList.add("valid");
-  } else {
+  } else { //Si la contraseña NO contiene mayúsculas
     mayuscula.classList.remove("valid");
     mayuscula.classList.add("invalid");
   }
 
   // Validar números
-  var numeros = /[0-9]/g;
-  if (input.value.match(numeros)) {
+  var numeros = /[0-9]/g; 
+  if (input.value.match(numeros)) { //Si la contraseña contiene números del 0 al 9.
     numero.classList.remove("invalid");
     numero.classList.add("valid");
-  } else {
+  } else { //Si la contraseña NO contiene números del 0 al 9.
     numero.classList.remove("valid");
     numero.classList.add("invalid");
   }
 
   // Validar longitud
-  if (input.value.length >= 8) {
+  if (input.value.length >= 8) { //Si la contraseña tiene que tener más de 8 carácteres
     longitud.classList.remove("invalid");
     longitud.classList.add("valid");
-  } else {
+  } else { //Si la contraseña NO tiene que tener más de 8 carácteres
     longitud.classList.remove("valid");
     longitud.classList.add("invalid");
   }
 
   // Mostrar / Ocultar botón submit
+  // Comprovación de que la contraseña cumple todas las directrices de validación
   if (
   minuscula.classList.contains("valid") &&
   mayuscula.classList.contains("valid") &&
   numero.classList.contains("valid") &&
   longitud.classList.contains("valid")
-) {
-  document.getElementById("botonSubmit").style.display = "block";
-  document.getElementById("mensaje").style.display = "none";
-} else {
-  document.getElementById("botonSubmit").style.display = "none";
+) { //Si se cumplen todas las validaciones
+  document.getElementById("botonSubmit").style.display = "block";  //El botón Submit se vuelve visible
+  document.getElementById("mensaje").style.display = "none"; // El mensaje de validación deja de ser visible
+} else { //Si NO se cumplen todas las validaciones
+  document.getElementById("botonSubmit").style.display = "none"; // El botón Submit deja de ser visible
 }
 };
