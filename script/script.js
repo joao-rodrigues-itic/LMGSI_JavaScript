a48e0455408eea9a6224fc6022bbdee2667e08b5
joaorodskt <144625686+joaorodskt@users.noreply.github.com>
2026-02-02 11:49:21 +0100

    ocultar/mostrar contrasenya

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-
-
 document.getElementById("esconder").addEventListener("change", function() {
     let contrasena = document.getElementById("contrasena");
 
     if (contrasena.type === 'password') {
         contrasena.setAttribute('type', 'text');
+    }else {
+        contrasena.setAttribute('type', 'password');
     }
 });
